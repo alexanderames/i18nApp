ec317f7a287213db3cc30a339d639ed1e4a23c44
alexander ames <alexanderthomasames@gmail.com>
2017-09-12 16:28:33 -0700

    set up translation tables

--- src/client/app/core/config.js
+++ src/client/app/core/config.js
@@ -21,9 +21,39 @@
 
   core.config(configure);
 
-  configure.$inject = ['$logProvider', 'routerHelperProvider', 'exceptionHandlerProvider'];
+  configure.$inject = ['$logProvider', 'routerHelperProvider', 'exceptionHandlerProvider', '$translateProvider'];
   /* @ngInject */
-  function configure($logProvider, routerHelperProvider, exceptionHandlerProvider) {
+  function configure($logProvider, routerHelperProvider, exceptionHandlerProvider, $translateProvider) {
+
+    var english = {
+      "Title": "Internationalization",
+      "Language": "Language",
+      "Languages": {
+        "English": "English",
+        "Spanish": "Spanish",
+        "French": "French"
+      },
+      "Created_By": "Created by Alexander Ames",
+      "First_Name": "First Name",
+      "Last_Name": "Last Name",
+      "Age": "Age",
+      "Location": "Location",
+      "Messages": "Messages",
+      "People": "People",
+      "Splash_Msg": "Loading...",
+      "Message_Count": "{messageCount, plural, =0{No Messages} one{1 Message} other{# Messages}",
+      "Conference_Date": "May 18 - 19, 2015",
+      "Dashboard": "Dashboard",
+      "Admin": "Admin",
+      "Greeting": "{{name}} is logged in",
+      "Admin_Message": "What up playa",
+      "Activation_Dash": "Activated Dashboard View",
+      "Activation_Admin": "Activated Admin View"
+    };
+
+    $translateProvider.translations('en', english);
+    $translateProvider.preferredLanguage('en');
+
     if ($logProvider.debugEnabled) {
       $logProvider.debugEnabled(true);
     }
